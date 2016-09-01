39b75ba7503afa5d89aeda1b5f3e9d0ae3473a5a
Patrik Korzinski <patrik.korzinski@gmail.com>
2016-09-01 16:03:30 -0700

    Added npm package dependencies, added ui-server framework.

---
+++ ui-server/ui-server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+//var bookshelf = require('bookshelf');
+
+//var db = ;
+
+var app = express();
+
+var port = process.env.PORT || 4568;
+
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(express.static(__dirname + '/public'));
+
+require('./ui-routes.js')(app);
+
+app.listen(port);
+console.log('Server listening on port ' + port);
+
+module.exports = app;
