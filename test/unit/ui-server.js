cf963ddb3135453cf16dcf31b9ca8f4ce5e29f51
Jim Yang <jimyyang@gmail.com>
2016-09-06 13:43:34 -0400

    (test) Message should be a function

--- test/unit/ui-server.js
+++ test/unit/ui-server.js
@@ -2,9 +2,18 @@ var chai = require('chai');
 var assert = chai.assert;
 var should = chai.should();
 var expect = chai.expect;
+var path = require('path')
+
+var Message = require(path.join(__dirname + '/../../db/models', 'message.js'))
 
 describe('A failing test to test test suite', function() {
   beforeEach(function(){});
   afterEach(function(){});
   it('should fail', function(){ expect(true).to.be.false; })
 });
+
+describe('Message Model test suite', function(){
+  it ('should be a function', function() {
+    Message.should.be.a('function')
+  })
+})
