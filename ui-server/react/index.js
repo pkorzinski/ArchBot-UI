ee0055a4c123e5ec4f3b7faba9b4cd635256c4b2
Jim Yang <jimyyang@gmail.com>
2016-09-06 16:29:56 -0400

    adding ejs views and react component views from server-side

---
+++ ui-server/react/index.js
@@ -0,0 +1,7 @@
+var React = require("react")
+var IndexComponent = React.createClass({
+    render:function(){
+        return React.createElement('h1', null, 'Hello Doge');
+    }
+});
+module.exports = IndexComponent;
