2b72c905230ff2433db5f78a1d5c10837646fabd
Patrik Korzinski <patrik.korzinski@gmail.com>
2016-09-07 10:06:57 -0700

    Webpack experimentation

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entry: "./public/scripts/Index.jsx",
+    output: {
+        path: __dirname + "/public/build",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.jsx$/,
+              loader: "babel",
+              exclude: /node_modules/ }
+        ]
+    },
+};
